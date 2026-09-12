faa6d5f71d16ca159f561d288a4212e64270ccb4
pea-sys <pea98258@gmail.com>
2026-09-12 21:50:55 +0900

    component test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
 
 export const App = () => {
-  return (
-    <main>
-      <h1>frontend-test-gihyo-book-sample</h1>
-      <p>動作確認用のサンプルアプリケーションです。</p>
-    </main>
-  );
+	return (
+		<main>
+			<h1>frontend-test-gihyo-book-sample</h1>
+			<p>動作確認用のサンプルアプリケーションです。</p>
+		</main>
+	);
 };
